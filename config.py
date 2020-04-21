3544e9dd5e38d872e7ccbec5bacc148dff61ee22
shiningchicken <jakapaulene@signetproperties.ph>
2020-04-21 21:26:21 +0800

    Finished Flask Tutorial Chapter 7

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'example-key'
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['csjakapaulene@gmail.com']
